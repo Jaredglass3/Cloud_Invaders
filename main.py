e0a4f71af9fff261d82912306eb980b9696b5d24
zgamble30 <zgamble30@gmail.com>
2023-04-22 16:52:58 -0500

    Added end game scenario and accounted for a few more edge cases

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from pygame.locals import *
+from enum import Enum
 
 # Initialize Pygame
 pygame.init()
@@ -48,9 +49,13 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.speed = 1
+        self.direction = 1
 
     def update(self):
-        self.rect.move_ip(0, self.speed)
+        self.rect.move_ip(self.speed * self.direction, 0)
+        if self.rect.right >= WIDTH or self.rect.left <= 0:
+            self.direction *= -1
+            self.rect.move_ip(0, self.speed)
 
 class Lightning(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -66,53 +71,72 @@ class Lightning(pygame.sprite.Sprite):
         if self.rect.y < 0:
             self.kill()
 
-# Create player and sprite groups
-player = Player(WIDTH // 2, HEIGHT - 50, 50, 50)
-enemies = pygame.sprite.Group()
-lightning_bolts = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
+# Game state enum
+class GameState(Enum):
+    PLAYING = 0
+    GAME_OVER = 1
+    VICTORY = 2
 
-# Create a 10x10 grid of enemies
-ENEMY_SPACING = 80
-for i in range(10):
-    for j in range(10):
-        enemy = Enemy(ENEMY_SPACING * i, ENEMY_SPACING * j, 50, 50)
-        all_sprites.add(enemy)
-        enemies.add(enemy)
+# Initialize game state
+game_state = GameState.PLAYING
 
 # Main game loop
-running = True
-while running:
+while True:
     clock.tick(30)
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            running = False
+            pygame.quit()
         if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                lightning = Lightning(player.rect.x + player.rect.width // 2 - 5, player.rect.y)
-                all_sprites.add(lightning)
-                lightning_bolts.add(lightning)
+            if game_state == GameState.PLAYING:
+                if event.key == K_SPACE:
+                    lightning = Lightning(player.rect.x + player.rect.width // 2 - 5, player.rect.y)
+                    all_sprites.add(lightning)
+                    lightning_bolts.add(lightning)
+            elif game_state in (GameState.GAME_OVER, GameState.VICTORY):
+                if event.key == K_r:
+                    # Restart the game
+                    player = Player(WIDTH // 2, HEIGHT - 50, 50, 50)
+                    enemies = pygame.sprite.Group()
+                    lightning_bolts = pygame.sprite.Group()
+                    all_sprites = pygame.sprite.Group()
+                    all_sprites.add(player)
 
-    pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys)
+                    for i in range(10):
+                        for j in range(10):
+                            enemy = Enemy(ENEMY_SPACING * i, ENEMY_SPACING * j, 50, 50)
+                            all_sprites.add(enemy)
+                            enemies.add(enemy)
 
-    for lightning in lightning_bolts:
-        lightning.update()
-        hit_enemies = pygame.sprite.spritecollide(lightning, enemies, True)
-        if hit_enemies:
-            lightning.kill()
+                    game_state = GameState.PLAYING
+                elif event.key == K_q:
+                    pygame.quit()
 
-    for enemy in enemies:
-        enemy.update()
-        if pygame.sprite.collide_rect(player, enemy):
-            running = False
-            print("Player was hit!")
+    if game_state == GameState.PLAYING:
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
 
-    screen.blit(BACKGROUND, (0, 0))
-    for entity in all_sprites:
-        screen.blit(entity.image, entity.rect)
-    pygame.display.flip()
+        for lightning in lightning_bolts:
+            lightning.update()
+            hit_enemies = pygame.sprite.spritecollide(lightning, enemies, True)
+            if hit_enemies:
+                lightning.kill()
+                if not enemies:
+                    print("All enemies defeated!")
+                    game_state = GameState.VICTORY
 
-pygame.quit()
+        for enemy in enemies:
+            enemy.update()
+            if pygame.sprite.collide_rect(player, enemy):
+                print("Player was hit!")
+                game_state = GameState.GAME_OVER
+
+        screen.blit(BACKGROUND, (0, 0))
+        for entity in all_sprites:
+            screen.blit(entity.image, entity.rect)
+        pygame.display.flip()
+
+    if game_state == GameState.GAME_OVER:
+        # Display "Game Over" message and instructions
+        font = pygame.font.Font(None, 36)
+        text = font.render("Game Over! Press
